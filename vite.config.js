51f43a8576b0df1e7d3dde1641cea002e1ca1fce
pidata-gokhan-bahadir <firat-bahadir@hotmail.com>
2024-08-12 20:50:34 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const BASE_PATH = process.env.BASE_PATH || '';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: BASE_PATH,
   plugins: [react({babel:{plugins:["module:@preact/signals-react-transform"]}})],
 })
