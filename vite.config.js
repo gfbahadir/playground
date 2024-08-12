bb8c32c95d6b35da6eaf1ba8f35f19b72a61eaf8
pidata-gokhan-bahadir <firat-bahadir@hotmail.com>
2024-08-12 20:58:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig((command)=>({
-  base:"/playground/",
+export default defineConfig(({ command, mode, isSsrBuild, isPreview })=>({
+  base:command === 'serve'? "":"/playground/",
   plugins: [react({babel:{plugins:["module:@preact/signals-react-transform"]}})],
 }))
